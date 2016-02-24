85fc709d85c2a16a13418a364a51a855d6d9ae90
Jacoub Bondre <jacoub.bondre@redlioncanada.com>
2016-02-24 04:44:21 -0500

    go away

--- app/landing.masthead.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-    selector: 'masthead',
-    templateUrl:'app/views/masthead.view.html'
-})
-export class AppMasthead { }
--- app/models/products.model.ts
+++
@@ -1,7 +0,0 @@
-export class ProductModel{
-    
-    constructor(public prodImage:string = "";
-                public prodName:string = "";
-                public prodDescription:string = "";
-                ){}
-}
