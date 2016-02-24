46de58e2dd9c470a822d310a31475b0bdbbe2194
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-24 12:05:22 -0500

    Revert "Added banner and learn-more button."
    This reverts commit d5755c151c3624c835bcf25884e26e8ec8c9e15e.

--- app/landing.learn-more.button.ts
+++
@@ -1,9 +0,0 @@
-import {Component, Input, Inject} from 'angular2/core'
-
-@Component({
-    selector: 'learn-more-button',
-    templateUrl: 'app/views/landing.learn-more.button.view.html'
-})
-export class LearnMoreButton {
-	@Input() link
-}
