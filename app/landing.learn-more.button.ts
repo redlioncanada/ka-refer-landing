d5755c151c3624c835bcf25884e26e8ec8c9e15e
Stephen Poole <stephen.poole@redlioncanada.com>
2016-02-24 11:22:34 -0500

    Added banner and learn-more button.

---
+++ app/landing.learn-more.button.ts
@@ -0,0 +1,9 @@
+import {Component, Input, Inject} from 'angular2/core'
+
+@Component({
+    selector: 'learn-more-button',
+    templateUrl: 'app/views/landing.learn-more.button.view.html'
+})
+export class LearnMoreButton {
+	@Input() link
+}
